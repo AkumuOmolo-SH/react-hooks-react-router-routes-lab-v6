7816ae450a2207c6a2941217b616fde04834151b
AkumuOmolo <akumu.omoloh@gmail.com>
2025-07-28 17:50:02 +0300

    Adds codegrade

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,29 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
 
 
-const routes = [
-   
-  ];
+import Home from "./pages/Home";
+import Directors from "./pages/Directors";
+import Actors from "./pages/Actors";
+import Movie from "./pages/Movie";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",        // Home route
+    element: <Home />
+  },
+  {
+    path: "/directors",
+    element: <Directors />
+  },
+  {
+    path: "/actors",
+    element: <Actors />
+  },
+  {
+    path: "/movie/:id",  
+    element: <Movie />
+  }
+]);
 
 export default routes;
